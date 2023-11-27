06350e4000ec8cf725615dbc5d34d0c5a4eb4a2e
Robert Carr <mrrobertcarr@gmail.com>
2023-11-27 20:01:08 +0000

    Created Console app

--- suggestionbox.api/obj/Debug/net8.0/suggestionbox.api.AssemblyInfo.cs
+++ suggestionbox.api/obj/Debug/net8.0/suggestionbox.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("suggestionbox.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5817389076e3c590703ced700a41149f69737123")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("suggestionbox.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("suggestionbox.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
