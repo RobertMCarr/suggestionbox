94f68adbf69407119c96951b7cfa66de6f656c39
Robert Carr <mrrobertcarr@gmail.com>
2024-04-23 00:14:38 +0100

    Added Email & Password Login.
    Needs:
    - logout
    - failed login exception handling
    -

--- suggestionbox.ui/suggestionbox/lib/main.dart
+++ suggestionbox.ui/suggestionbox/lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
-import 'pages/login_page.dart';
+import 'package:suggestionbox/pages/auth_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+const supabaseUrl = 'https://ohtpodvpzzwbqzmqnxto.supabase.co';
+const supabaseAnonKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9odHBvZHZwenp3YnF6bXFueHRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2Njg1NzcsImV4cCI6MjAyODI0NDU3N30.3Ap06ZKOgf3RpwYu3Dk18drbM9GAVWoNHx3uorYKIj4';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
+
   runApp(const SuggestionBoxApp());
 }
 
@@ -11,9 +23,9 @@ class SuggestionBoxApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: AuthPage(),
     );
   }
 }
